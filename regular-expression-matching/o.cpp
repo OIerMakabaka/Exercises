5896037bc5b9c5ca82d053d33e3dd744928d0aae
Makabaka <1842396359@qq.com>
2021-10-04 20:29:01 +0800

    Initialize

---
+++ regular-expression-matching/o.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <cstdio>
+#include <cstring>
+
+using namespace std;
+
+int main() {
+
+	// freopen("o.in", "r", stdin);
+	// freopen("o.out", "w", stoudt);
+
+	
+
+
+
+
+
+	return 0;
+}
